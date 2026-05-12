1dea4e0c27d8bfb3a55c29e335ad26e91c0b44e4
Yuri Hadid <hadidyuri04@gmail.com>
2026-05-12 21:01:28 +0300

    feat(task-model): add task model and migrations

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    title= models.CharField(max_length=100)
+    assigned_to=models.ForeignKey(User, on_delete=models.CASCADE,related_name='assigned_tasks')
+    created_by=models.ForeignKey(User,on_delete=models.CASCADE,related_name='created_tasks')
+    description=models.TextField(blank=True,null=True)
+    Status_choice=[('Pending','Pending'),('In Progress','In Progress'),('Completed','Completed'),]
+    status=models.CharField(max_length=20,choices=Status_choice,default='Pending')
+    created_date=models.DateTimeField(auto_now_add=True)
+    due_date=models.DateField()
+    def __str__(self):
+        return self.title
