a07614513e74b381fa06bc8695867feaeb092c57
Yuri Hadid <hadidyuri04@gmail.com>
2026-05-12 22:07:48 +0300

    docs: confirm database schema mapping in PostgreSQL

---
+++ task_manager/tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # This matches: http://127.0.0.1:8000/tasks/
+    path('', views.task_list, name='task_list'),
+    
+    # This matches: http://127.0.0.1:8000/tasks/create/
+    path('create/', views.task_create, name='task_create'),
+]
