98c18c08e6128d4d1aa4f750149348379cf52742
Yuri Hadid <hadidyuri04@gmail.com>
2026-05-12 21:17:29 +0300

    feat(database): verify migration history in django_migrations table

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_db',        # Must match the name in pgAdmin
+        'USER': 'postgres',       # Default username
+        'PASSWORD': 'Yuri@2004', 
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
